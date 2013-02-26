464e29f968fdf144f8b2a1fb58372c3b8c670135
bussiere <bussiere@gmail.com>
2013-02-26 12:19:02 +0100

    rajout

--- Jackpoint/jack/models.py
+++ Jackpoint/jack/models.py
@@ -58,25 +58,53 @@ class Statut(models.Model):
     #SuperAdmin
     #Admin
 
+class Communaute(models.Model):
+    Nom = models.CharField(max_length=256)
+
 class UserProfile(models.Model):  
     user = models.OneToOneField(User)  
     #other fields here
-    Skills = models.ManyToManyField("SkillUser", blank=True, null=True)
-    TagsPublic = models.ManyToManyField("tag.Tag", blank=True, null=True)
-    Items = models.ManyToManyField("ItemUser", blank=True, null=True)
-    Caracs = models.ManyToManyField("CaracUser", blank=True, null=True)
+    Skills = models.ManyToManyField("SkillUser", blank=True, null=True,related_name="SkillUser")
+    Tags = models.ManyToManyField("tag.Tag", blank=True, null=True,related_name="Tag")
+    Items = models.ManyToManyField("ItemUser", blank=True, null=True,related_name="ItemUser")
+    Caracs = models.ManyToManyField("CaracUser", blank=True, null=True,related_name="CaracUser")
     Pseudo = models.CharField(max_length=256)
     Bio = models.TextField()
     Email = models.EmailField()
     Avatar = models.ImageField(upload_to='Avatar')
     Finished = models.BooleanField(default=False)
-    Statut = models.ManyToManyField("Statut", blank=True, null=True)
+    Statut = models.ManyToManyField("Statut", blank=True, null=True,related_name="StatutUser")
+    Communaute = models.ManyToManyField("Communaute", blank=True, null=True,related_name="CommunauteUser")
     InvitationAccepted = models.OneToOneField("invitation.Invitation",related_name="InvitationAccepted", blank=True, null=True)
     InvitationGiven = models.ManyToManyField("invitation.Invitation",related_name="InvitationGiven", blank=True, null=True)
     Administration = models.ManyToManyField("administration.Administration",null=True, blank=True,related_name="Jack_UserProfile_Admin")
     Date = models.DateTimeField(auto_now_add=True, blank=True)
     def __str__(self):  
           return "%s's profile" % self.user  
+
+
+class UserProfileTemp(models.Model):  
+    user = models.OneToOneField(User)  
+    #other fields here
+    Skills = models.ManyToManyField("SkillUser", blank=True, null=True,related_name="SkillUserTemp")
+    Tags = models.ManyToManyField("tag.Tag", blank=True, null=True,related_name="TagTemp")
+    Items = models.ManyToManyField("ItemUser", blank=True, null=True,related_name="ItemUserTemp")
+    Caracs = models.ManyToManyField("CaracUser", blank=True, null=True,related_name="CaracUserTemp")
+    Pseudo = models.CharField(max_length=256)
+    Bio = models.TextField()
+    Email = models.EmailField()
+    Avatar = models.ImageField(upload_to='Avatar')
+    Finished = models.BooleanField(default=False)
+    Communaute = models.ManyToManyField("Communaute", blank=True, null=True,related_name="CommunauteUserTemp")
+    Statut = models.ManyToManyField("Statut", blank=True, null=True,related_name="StatutUserTemp")
+    InvitationAccepted = models.OneToOneField("invitation.Invitation",related_name="InvitationAcceptedTemp", blank=True, null=True)
+    InvitationGiven = models.ManyToManyField("invitation.Invitation",related_name="InvitationGivenTemp", blank=True, null=True)
+    Administration = models.ManyToManyField("administration.Administration",null=True, blank=True,related_name="Jack_UserProfileTemp_Admin")
+    Date = models.DateTimeField(auto_now_add=True, blank=True)
+    def __str__(self):  
+          return "%s's profile" % self.user
+
+
 class Createur(models.Model):  
     User =  models.ForeignKey(UserProfile, unique=False, null=True, blank=True)
     Date = models.DateTimeField(auto_now_add=True, blank=True)
