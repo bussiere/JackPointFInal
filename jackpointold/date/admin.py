3f0c6f72e9625a12f748a0bb98df13105f758410
Bussiere <bussiere@gmail.com>
2013-07-04 16:20:16 +0200

    nettoyage

--- jackpointold/date/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from date.models import DateJourHoraire,Horaire,Plage
-admin.site.register(DateJourHoraire)
-admin.site.register(Horaire)
-admin.site.register(Plage)
