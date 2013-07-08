c414480fee519e68aece68068e941278fe10cf0a
bussiere <bussiere@gmail.com>
2013-07-08 13:10:41 +0200

    rajout eclipe

--- Jackpointnew/settings.py
+++ Jackpointnew/settings.py
@@ -88,7 +88,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = 'dkfjsdlkfjsdlkfjlsdkjglfdkgflkgjdlfkgjdflkgjdfgdfgdfdfg87687687687JK23B3KJN?AN'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
